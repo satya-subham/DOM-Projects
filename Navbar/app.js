e3a0939d0e7f96f000c5bed90e6295b3058b5d16
satya-subham <satyasubham85@gmail.com>
2022-08-11 13:03:50 +0530

    navbar

---
+++ Navbar/app.js
@@ -0,0 +1,9 @@
+let NavMenu = document.querySelector('.nav-toggle');
+let links = document.querySelector('.links');
+
+NavMenu.addEventListener('click', (event)=>{
+    console.log(event.target);
+    // if(links.style.display == 'inline'){
+    //     links.style.display = 'none';
+    // }
+})
