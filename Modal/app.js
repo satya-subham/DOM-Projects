efef24b84b723215c0ec5d62a3e1cea78a745ee8
satya-subham <satyasubham85@gmail.com>
2022-08-11 17:00:53 +0530

    changes

--- Modal/app.js
+++ Modal/app.js
@@ -1,11 +1,23 @@
+let body = document.querySelector('body');
 let main = document.querySelector('.main');
 let mainBtn = document.querySelector('.main-btn');
 let open = document.querySelector('.open');
 let openBtn = document.querySelector('.open-btn');
 
 mainBtn.addEventListener('click', (event)=>{
-    // console.log(event.target);
-    if(main.style.visibility == 'visible'){
+    // console.log(event.target.innerText);
+    if(event.target.innerText == 'OPEN MODAL'){
         main.style.visibility = 'hidden'
+        open.style.visibility = 'visible'
+        body.style.backgroundColor = '#333'
+    }
+
+});
+
+openBtn.addEventListener('click', (event)=>{
+    if(event.target.innerText == ''){
+        open.style.visibility = 'hidden';
+        main.style.visibility = 'visible'
+        body.style.backgroundColor = 'cadetblue'
     }
 })
